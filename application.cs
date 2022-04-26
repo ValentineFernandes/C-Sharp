f1ad44497c5a731dd77caa56624f157fe9845831
Valentine Fernandes <valentineinfantinejohn@gmail.com>
2022-04-26 01:05:47 -0800

    Create application.cs

---
+++ application.cs
@@ -0,0 +1,13 @@
+using System;
+
+namespace MyApplication
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      Console.Write("Hello World! ");
+      Console.Write("I will print on the same line.");
+    }
+  }
+}
