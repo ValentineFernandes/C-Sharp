47ff0aab308b89f194da76211d6d9bddb600e224
Valentine Fernandes <valentineinfantinejohn@gmail.com>
2022-04-26 01:12:27 -0800

    Create manyvariables.cs

---
+++ manyvariables.cs
@@ -0,0 +1,13 @@
+using System;
+
+namespace MyApplication
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      int x = 5, y = 6, z = 50;
+      Console.WriteLine(x + y + z);
+    }
+  }
+}
