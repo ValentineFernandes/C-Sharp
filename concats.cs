79bf724adf146f33695ee5b31af56c6f5275ed0e
Valentine Fernandes <valentineinfantinejohn@gmail.com>
2022-04-26 01:11:13 -0800

    Create concats.cs

---
+++ concats.cs
@@ -0,0 +1,15 @@
+using System;
+
+namespace MyApplication
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      string firstName = "John ";
+      string lastName = "Doe";
+      string fullName = firstName + lastName;
+      Console.WriteLine(fullName);
+    }
+  }
+}
