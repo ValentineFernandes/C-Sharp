9b96a0bcd3c46e7e05bd7df00c4b3c7ddc89917d
Valentine Fernandes <valentineinfantinejohn@gmail.com>
2022-04-26 01:22:52 -0800

    Create data.cs

---
+++ data.cs
@@ -0,0 +1,19 @@
+using System;
+
+namespace MyApplication
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      int myInt = 10;
+      double myDouble = 5.25;
+      bool myBool = true;
+
+      Console.WriteLine(Convert.ToString(myInt));    // Convert int to string
+      Console.WriteLine(Convert.ToDouble(myInt));    // Convert int to double
+      Console.WriteLine(Convert.ToInt32(myDouble));  // Convert double to int
+      Console.WriteLine(Convert.ToString(myBool));   // Convert bool to string
+      }
+  }
+}
