20deb995c3d3aeb0e19b9fbe02b2d8b33b000259
Valentine Fernandes <valentineinfantinejohn@gmail.com>
2022-04-26 01:08:06 -0800

    Create var.cs

---
+++ var.cs
@@ -0,0 +1,13 @@
+using System;
+
+namespace MyApplication
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      string name = "John";
+      Console.WriteLine(name);  
+     }
+  }
+}
