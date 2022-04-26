416ecc2deff7fad0d03114f3e3890c95cef79a4a
Valentine Fernandes <valentineinfantinejohn@gmail.com>
2022-04-26 01:07:20 -0800

    Create multiline-comment.cs

---
+++ multiline-comment.cs
@@ -0,0 +1,14 @@
+using System;
+
+namespace HelloWorld
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      /* The code below will print the words Hello World
+      to the screen, and it is amazing */
+      Console.WriteLine("Hello World!");    
+     }
+  }
+}
