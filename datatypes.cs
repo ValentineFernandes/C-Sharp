62117b1083e07676105273811e113d1125eaa0be
Valentine Fernandes <valentineinfantinejohn@gmail.com>
2022-04-26 01:14:36 -0800

    Create datatypes.cs

---
+++ datatypes.cs
@@ -0,0 +1,21 @@
+using System;
+
+namespace MyApplication
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      int myNum = 5;               // integer (whole number)
+      double myDoubleNum = 5.99D;  // floating point number
+      char myLetter = 'D';         // character
+      bool myBool = true;          // boolean
+      string myText = "Hello";     // string
+      Console.WriteLine(myNum);
+      Console.WriteLine(myDoubleNum);
+      Console.WriteLine(myLetter);
+      Console.WriteLine(myBool);
+      Console.WriteLine(myText);
+    }
+  }
+}
