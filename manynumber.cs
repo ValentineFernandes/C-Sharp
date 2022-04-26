5042c57709324c6183dfa84fe42cf87d5d1d2b47
Valentine Fernandes <valentineinfantinejohn@gmail.com>
2022-04-26 01:13:10 -0800

    Create manynumber.cs

---
+++ manynumber.cs
@@ -0,0 +1,14 @@
+using System;
+
+namespace MyApplication
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      int x, y, z;
+      x = y = z = 50;
+      Console.WriteLine(x + y + z);
+    }
+  }
+}
