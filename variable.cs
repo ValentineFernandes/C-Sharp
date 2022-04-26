fb3e1f0f989055b75116ac94cc56ea10eadad515
Valentine Fernandes <valentineinfantinejohn@gmail.com>
2022-04-26 01:08:52 -0800

    Create variable.cs

---
+++ variable.cs
@@ -0,0 +1,13 @@
+using System;
+
+namespace MyApplication
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      int myNum = 15;
+      Console.WriteLine(myNum);
+     }
+  }
+}
