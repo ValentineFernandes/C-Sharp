ed19dcffe3f1ad441ee7e69dcd93e9c7d12ce165
Valentine Fernandes <valentineinfantinejohn@gmail.com>
2022-04-26 01:09:57 -0800

    Create number.cs

---
+++ number.cs
@@ -0,0 +1,14 @@
+using System;
+
+namespace MyApplication
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      int myNum = 15;
+      myNum = 20;
+      Console.WriteLine(myNum);
+    }
+  }
+}
