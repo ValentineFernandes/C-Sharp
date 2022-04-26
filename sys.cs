056955aa4b91cd890a7b038ff23cd413f9953278
Valentine Fernandes <valentineinfantinejohn@gmail.com>
2022-04-26 01:11:54 -0800

    Create sys.cs

---
+++ sys.cs
@@ -0,0 +1,14 @@
+using System;
+
+namespace MyApplication
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      int x = 5;
+      int y = 6;
+      Console.WriteLine(x + y);
+    }
+  }
+}
