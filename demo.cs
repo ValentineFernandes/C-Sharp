3862c113961903c174721dc0705858bdf80a7e30
Valentine Fernandes <valentineinfantinejohn@gmail.com>
2022-04-26 01:03:39 -0800

    Create demo.cs

---
+++ demo.cs
@@ -0,0 +1,14 @@
+using System;
+
+namespace MyApplication
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      Console.WriteLine("Hello World!");
+      Console.WriteLine("I am Learning C#");
+      Console.WriteLine("It is awesome!");
+    }
+  }
+}
