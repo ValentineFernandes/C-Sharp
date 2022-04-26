00123e954a29a0522c15dbe18896ed7c19212dc2
Valentine Fernandes <valentineinfantinejohn@gmail.com>
2022-04-26 01:24:34 -0800

    Create add.cs

---
+++ add.cs
@@ -0,0 +1,13 @@
+using System;
+
+namespace MyApplication
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      int x = 100 + 50;
+      Console.WriteLine(x);  
+     }
+  }
+}
