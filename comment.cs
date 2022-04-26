1cce1cae8bff43b340b7308f8571b89cb49b7988
Valentine Fernandes <valentineinfantinejohn@gmail.com>
2022-04-26 01:06:29 -0800

    Create comment.cs

---
+++ comment.cs
@@ -0,0 +1,13 @@
+using System;
+
+namespace HelloWorld
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      // This is a comment
+      Console.WriteLine("Hello World!");    
+     }
+  }
+}
