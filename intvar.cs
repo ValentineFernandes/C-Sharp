e380afe7ce5e03751e216504de07507be1b3f6d1
Valentine Fernandes <valentineinfantinejohn@gmail.com>
2022-04-26 01:15:17 -0800

    Create intvar.cs

---
+++ intvar.cs
@@ -0,0 +1,13 @@
+using System;
+
+namespace MyApplication
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      int myNum = 100000;
+      Console.WriteLine(myNum);
+    }
+  }
+}
